3d23a6c36d7c0c2fbe421c1239c77d7e1247d5da
Duccio Rocchini <ducciorocchini@gmail.com>
2025-12-29 12:05:26 +0100

    Update script dependencies and output plots

--- Code/patchwork_synthetic.R
+++ Code/patchwork_synthetic.R
@@ -1,4 +1,4 @@
-# Tis script can be run after the scripts synthetic_size.R and synthetic_bands.R have been run. 
+# Tis script can be run after the scripts synthetic_size.R, synthetic_bands.R and synthetic_clusters.R have been run. 
 # It will join the two resulting plots in a single image.
 
 library(patchwork)
@@ -9,7 +9,10 @@ p_runtime_size
 # Output from synthetic_bands.R
 p_runtime_bands
 
-p_runtime_size + p_runtime_bands 
+# Output from synthetic_clusters.R
+p_runtime_clusters
+
+p_runtime_size + p_runtime_bands + p_runtime_clusters
 # + plot_annotation(tag_levels = "A)") 
-# plot_annotation() can be used to put labels A) and B) to plots, but in this case it might be misleading due to the number of bands called B
+# plot_annotation() can be used to put labels A), B) and C) to plots, but in this case it might be misleading due to the number of bands called B
  
